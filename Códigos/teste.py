910627d9f64e9d84b6b5b082d4c340ea1d4a9dd4
mister-rafael <93235866+mister-rafael@users.noreply.github.com>
2022-01-03 23:18:16 -0300

    Remoção e inclusão
    Adicionado e excluído itens de testes

--- Códigos/teste.py
+++ Códigos/teste.py
@@ -1,6 +1,7 @@
 from model import *
 from crud import *
-
+from interface_peca import (CadastroPeca)
+from interface_material import (CadastroMaterial)
 #ADCIONAR DADOS EM:
 
 #PEÇA
@@ -28,9 +29,27 @@ entradas = [
 	{'peca': '3','dataEntrada':'01/12/2021', 'qtd_entrada':'8'},
 	{'peca': '3','dataEntrada':'01/12/2021', 'qtd_entrada':'8'},
 ]
-
+materiais = [
+	{'nome': 'Massa corrida','und_medida':'Lata', 'qtd_estoque':'10', 'qtd_inicial':'0', 'qtd_entrada':'1', 'qtd_saida':'1', 'ultima_compra':'02/03/2021'},
+	{'nome': 'Tinta','und_medida':'Unidade', 'qtd_estoque':'10', 'qtd_inicial':'0', 'qtd_entrada':'1', 'qtd_saida':'1', 'ultima_compra':'02/04/2021'},
+	{'nome': 'Lixa','und_medida':'Unidade', 'qtd_estoque':'10', 'qtd_inicial':'0', 'qtd_entrada':'1', 'qtd_saida':'1', 'ultima_compra':'02/05/2021'},
+]
 
 #inserir(CategTamanho, categorias) #chamando um CRUD
-inserir(pecas, Peca) #chamando um CRUD
+#inserir(pecas, Peca) #chamando um CRUD
 #inserir(ControleEntrada, entradas) #chamando um CRUD
 #inserir(ItemVenda, vendas) #chamando um CRUD
+#inserir(materiais, Material)
+#consultar(ItemVenda)
+
+'''resultado = Peca.select(Peca.nome).join(ItemVenda)
+for i in resultado:
+	print(i)'''
+
+
+#tela = CadastroPeca()
+
+#tela.Iniciar()
+
+tela2 = CadastroMaterial()
+tela2.Iniciar()
