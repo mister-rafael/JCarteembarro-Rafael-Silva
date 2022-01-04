3217f0c808e8a01e6048a2f82fd3901fe752344d
mister-rafael <93235866+mister-rafael@users.noreply.github.com>
2022-01-03 23:24:42 -0300

    Atualização do inserir()
    Acrescentado o try e except

--- Códigos/crud.py
+++ Códigos/crud.py
@@ -1,14 +1,21 @@
 from model import *
-
+	
 def inserir(dados, tabela):
-	tabela.insert_many(dados).execute()
-
+	try:
+		tabela.insert_many(dados).execute()
+		print("Dados inseridos com sucesso.")
+	except:
+		print("Há dados repetidos")
+		return False
 def remover():
 	pass
 
 def alterar():
 	pass
 
-def consultar():
-	pass
+def consultar(tabela):
+	resultado = tabela.select()
+	for i in resultado:
+		print(i)
+	
 
