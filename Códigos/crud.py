d0aa1b9d4c474527e4c99bb2209b5782ce5f709a
mister-rafael <93235866+mister-rafael@users.noreply.github.com>
2022-02-10 17:00:13 -0300

    Crud
    Criação de novas funções para consulta no banco

--- Códigos/crud.py
+++ Códigos/crud.py
@@ -1,21 +1,51 @@
 from model import *
+
+
+def inserir(tabela, dados):
+	tabela.insert_many(dados).execute()
+
+#_________updates_____________________________
+
 	
-def inserir(dados, tabela):
-	try:
-		tabela.insert_many(dados).execute()
-		print("Dados inseridos com sucesso.")
-	except:
-		print("Há dados repetidos")
-		return False
-def remover():
-	pass
+#_________consultas__________________________
 
-def alterar():
-	pass
-
-def consultar(tabela):
-	resultado = tabela.select()
-	for i in resultado:
-		print(i)
+def listar_dados_estoquePeca(): #consulta o banco de dados e retorna os dados da yabela estoque peças
 	
+	consulta = Peca.select().join(CategTamanho)
+	
+	tabela = []
+	for peca in consulta:	
+		tabela.append([peca.id, peca.nome, peca.valor_unit, peca.tamanhoPeca_id.tamanhoPeca, peca.qtd_estoque, peca.qtd_inicial, peca.qtd_entrada, peca.qtd_saida])
+	
+	return tabela
 
+def listarDados_estoquePeca_porItem(condicao): #consulta o banco de dados e retorna os dados da yabela estoque peças
+	consulta = Peca.select().join(CategTamanho).where(Peca.id == condicao)
+	tabela = []
+	for peca in consulta:	
+		tabela.append([peca.id, peca.nome, peca.valor_unit, peca.tamanhoPeca_id.tamanhoPeca, peca.qtd_estoque, peca.qtd_inicial, peca.qtd_entrada, peca.qtd_saida])
+	return tabela
+
+def retornaTamanho(condicao):
+	consulta = CategTamanho.select().where(CategTamanho.tamanhoPeca == condicao).get()
+	return consulta
+
+def retornaControleEntrada(condicao):
+	consulta = Peca.select().join(Peca).where(ControleEntrada.peca_id == condicao)
+	tabela = []
+	for i in consulta:	
+		tabela.append([i.id, i.peca_id.nome])
+	return tabela	
+
+
+'''def Login(login, senha):
+	usuario_cadastrado = 'rafael'
+	senha_cadastrada = '1234'
+	if login == usuario_cadastrado and senha == senha_cadastrada:
+		print('Acesso liberado.')
+	elif login == usuario_cadastrado and senha != senha_cadastrada:
+		print('Senha incorreta.')
+	elif login != usuario_cadastrado and senha == senha_cadastrada:
+		print('Usuário invalido')
+	else:
+		print('Usuário e senha incorretos.')'''
